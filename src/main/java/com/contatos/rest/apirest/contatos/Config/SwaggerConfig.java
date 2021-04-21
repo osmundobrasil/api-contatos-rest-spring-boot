6b75e1238818e7580d769a1b309fb15752c37c40
ozzy brasil <ozzybrasil2016@gmail.com>
2021-04-20 20:13:11 -0400

    deploy da apirest-contatos v1.0

--- src/main/java/com/contatos/rest/apirest/contatos/Config/SwaggerConfig.java
+++ src/main/java/com/contatos/rest/apirest/contatos/Config/SwaggerConfig.java
@@ -1,15 +1,15 @@
 package com.contatos.rest.apirest.contatos.Config;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.google.common.base.Predicate;
+//import com.google.common.base.Predicate;
 
-import io.swagger.models.Contact;
+//import io.swagger.models.Contact;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.VendorExtension;
+//import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
